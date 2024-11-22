8fda3fe85ff4e39a29a5b5ce059d66847ef161c3
aryansaputra69 <ariyansaputra726@gmail.com>
2024-11-22 16:19:40 +0700

    Modul2

---
+++ resources/views/register.blade.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="zxx" class="no-js">
+
+<head>
+    <!-- Mobile Specific Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="{{ asset('assets/templates/user/img/fav.png') }}">
+
+    <!-- Meta Information -->
+    <meta name="author" content="CodePixar">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta charset="UTF-8">
+    <title>Merch Store</title>
+
+    <!-- CSS Files -->
+    <link rel="stylesheet" href="{{ asset('assets/templates/user/css/linearicons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/templates/user/css/owl.carousel.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/templates/user/css/themify-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/templates/user/css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/templates/user/css/nice-select.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/templates/user/css/nouislider.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/templates/user/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/templates/user/css/main.css') }}">
+</head>
+
+<body>
+    @include('sweetalert::alert')
+
+    <!--================ Login Box Area =================-->
+    <section class="login_box_area section_gap">
+        <div class="container">
+            <div class="row">
+                <!-- Image Section -->
+                <div class="col-lg-6">
+                    <div class="login_box_img">
+                        <img class="img-fluid" src="{{ asset('assets/templates/user/img/login.jpg') }}" alt="">
+                        <div class="hover">
+                            <p>There are advances being made in science and technology everyday, and a good example of this is the</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Form Section -->
+                <div class="col-lg-6">
+                    <div class="login_form_inner">
+                        <h3>Register new account</h3>
+                        <form class="row login_form" action="{{ route('post.register') }}" method="POST" id="contactForm" novalidate="novalidate">
+                            @csrf
+                            <!-- Name Input -->
+                            <div class="col-md-12 form-group">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Name'" value="{{ old('name') }}" required>
+                                @error('name')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <!-- Email Input -->
+                            <div class="col-md-12 form-group">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Email" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Email'" value="{{ old('email') }}" required>
+                                @error('email')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <!-- Password Input -->
+                            <div class="col-md-12 form-group">
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Password" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Password'" required>
+                                @error('password')
+                                    <span class="text-danger">{{ $message }}</span>
+                                @enderror
+                            </div>
+
+                            <!-- Submit Button -->
+                            <div class="col-md-12 form-group">
+                                <button type="submit" class="primary-btn">Create Account</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--================ End Login Box Area =================-->
+
+    <!-- JS Files -->
+    <script src="{{ asset('assets/templates/user/js/vendor/jquery-2.2.4.min.js') }}"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+    <script src="{{ asset('assets/templates/user/js/vendor/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/templates/user/js/jquery.ajaxchimp.min.js') }}"></script>
+    <script src="{{ asset('assets/templates/user/js/jquery.nice-select.min.js') }}"></script>
+    <script src="{{ asset('assets/templates/user/js/jquery.sticky.js') }}"></script>
+    <script src="{{ asset('assets/templates/user/js/nouislider.min.js') }}"></script>
+    <script src="{{ asset('assets/templates/user/js/jquery.magnific-popup.min.js') }}"></script>
+    <script src="{{ asset('assets/templates/user/js/owl.carousel.min.js') }}"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_GOOGLE_MAPS_API_KEY"></script>
+    <script src="{{ asset('assets/templates/user/js/gmaps.min.js') }}"></script>
+    <script src="{{ asset('assets/templates/user/js/main.js') }}"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,31 +1,33 @@
 <?php
 
-use App\Http\Controllers\ListController;
 use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Http\Controllers\User\UserController;
 
 // Guest Route
-Route::group(['middleware' => 'guest'], function() {
+Route::group(['middleware' => 'guest'], function () {
     Route::get('/', function () {
         return view('welcome');
     });
-    Route::post('/post-login', [AuthController::class, 'Login']);
+
+    Route::get('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/post-register', [AuthController::class, 'post_register'])->name('post.register');
+    Route::post('/post-login', [AuthController::class, 'login']);
 });
 
 // Admin Route
-Route::group(['middleware' => 'admin'], function() {
-    Route::get('/admin', function() {
-        return view('pages.admin.index');
-    })->name('admin.dashboard');
+Route::group(['middleware' => 'admin'], function () {
+    Route::get('/admin', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+
+    // Product Route
+    Route::get('/product', [ProductController::class, 'index'])->name('admin.product');
     Route::get('/admin-logout', [AuthController::class, 'admin_logout'])->name('admin.logout');
 });
 
 // User Route
-Route::group(['middleware' => 'web'], function() {
-    Route::get('/user', function() {
-        return view('pages.user.index');
-    })->name('user.dashboard');
+Route::group(['middleware' => 'web'], function () {
+    Route::get('/user', [UserController::class, 'index'])->name('user.dashboard');
     Route::get('/user-logout', [AuthController::class, 'user_logout'])->name('user.logout');
 });
-
-
